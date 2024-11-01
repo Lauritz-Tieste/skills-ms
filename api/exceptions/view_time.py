29a66838c610bf851e450270050b9b19c6b717e7
Lauritz Tieste <lauritz6000000@gmail.com>
2024-11-01 22:51:33 +0100

    Redefine data fetching

--- api/exceptions/view_time.py
+++ api/exceptions/view_time.py
@@ -6,8 +6,3 @@ class DataFetchError(APIException):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     detail = "Failed to fetch task data"
     description = "Failed to fetch task data from the database"
-
-class UnexpectedDataStructure(APIException):
-    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    detail = "Unexpected data structure"
-    description = "The data structure returned from the database was not as expected"
