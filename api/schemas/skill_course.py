95882f9eca7335bf8ac608c063562a8b3aad37ed
Lauritz Tieste <lauritz6000000@gmail.com>
2024-11-06 15:24:35 +0100

    Remove unused file

--- api/schemas/skill_course.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-
-from api.schemas.skill import SubSkill
-
-
-class SkillCourse(BaseModel):
-    skill_id: str = Field(max_length=256, description="ID of the skill")
-    skill: SubSkill = Field(description="ID of the skill")
-    course_id: str = Field(max_length=256, description="ID of the course")
